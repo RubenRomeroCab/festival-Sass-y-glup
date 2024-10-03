dda12e1574f87013f5a30cecdfaf2786c9320195
RubenRomeroCab <rubenromerodev@gmail.com>
2024-10-03 16:14:56 +0200

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+import {src , dest ,watch} from 'gulp';
+import * as dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+
+export function hola( done ){
+    console.log("hola desde gulpfile");
+    done()
+}
+
+const sass = gulpSass(dartSass)
+
+export function css(done){
+    src('src/scss/app.scss')
+    .pipe(sass().on('error',sass.logError))
+    .pipe(dest('dist/css'))
+
+    done()
+}
+export function dev(){
+    watch('src/scss/**/*.scss',css)
+}
+
+
+
+
