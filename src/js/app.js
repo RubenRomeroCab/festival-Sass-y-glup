392fae970fe70dbd25a7f090116b7d2d8825ef34
RubenRomeroCab <rubenromerodev@gmail.com>
2024-10-06 05:13:14 +0200

    Terminado
    terminada la galería y los enlaces

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,21 @@
 document.addEventListener("DOMContentLoaded",function(){
     crearGaleria();
+    navegacionFija();
 })
+
+function navegacionFija() {
+    const header = document.querySelector('.header');
+    const sobreFestival = document.querySelector('.sobre-festival');
+
+    window.addEventListener('scroll', function() {
+        if(sobreFestival.getBoundingClientRect().bottom<1){
+            header.classList.add('fixed')
+        }else{
+            header.classList.remove('fixed')
+        }
+    });
+   
+}
 
 function crearGaleria(){
     const galeria = document.querySelector('.galeria-imagenes')
@@ -11,17 +26,56 @@ function crearGaleria(){
 
         imagen.onclick = function(){
             mostrarImagen(i)
+          
         }
+
+
 
         galeria.appendChild(imagen);
     }
 }
 
 function mostrarImagen(i){
+
+    const imagen = document.createElement('IMG');
+    imagen.src= `src/img/gallery/full/${i}.jpg`;
+
+    //general modal
     const modal = document.createElement('DIV')
     modal.classList.add('modal')
-    modal.onclick(modal)
+   
+    //boton cerrar modal
+
+    const cerrarModalBtn = document.createElement('BUTTON')
+    cerrarModalBtn.textContent = 'X'
+    cerrarModalBtn.classList.add('btn-cerrar')
+    cerrarModalBtn.onclick = cerrarModal
+    
+    modal.appendChild(imagen)
+    modal.appendChild(cerrarModalBtn)
+
+
+
+   modal.onclick = cerrarModal;
+   modal.appendChild(imagen)
+      
+  
+  //agregar al html
 
     const body = document.querySelector('body')
+    body.classList.add('overflow-hidden')
+   
     body.appendChild(modal)
+}
+
+function cerrarModal(){
+    
+    const modal = document.querySelector('.modal')
+
+    const body = document.querySelector('body')
+    body.classList.remove('overflow-hidden')
+   
+    modal?.remove()
+  
+   
 }
